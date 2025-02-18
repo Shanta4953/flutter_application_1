1b84ddf1354dae82e6a1c438cca9eb94fb176f96
Shanta4953 <shanta15-4953@diu.edu.bd>
2025-02-18 11:01:03 +0600

    2nd

--- lib/main.dart
+++ lib/main.dart
@@ -6,40 +6,39 @@ void main() {
 
 class LabClass05 extends StatelessWidget {
   const LabClass05({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic Flutter UI-02",
+      title: "Basic Flutter UI - 02",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Basic Flutter UI-02"),
-          backgroundColor: Colors.blue[400],
-          centerTitle: true,
+          title: Text("Basic Flutter UI - 02"),
+          backgroundColor: Colors.blue,
         ),
-        body: Row(
-          children: [
-            Icon(
-             Icons.star,
-             size: 50, 
-            ),
-            Icon(
-             Icons.star,
-             size: 50, 
-            ),
-            Icon(
-             Icons.star,
-             size: 50, 
-            ),
-            Icon(
-             Icons.star,
-             size: 50, 
-            ),
-            Icon(
-             Icons.star_outline,
-             size: 50, 
-            ),
-          ],
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.ac_unit,
+                size: 30,
+                color: Colors.black,
+              ),
+              Icon(
+                Icons.account_tree,
+                size: 30,
+                color: Colors.black,
+              ),
+              Icon(
+                Icons.access_alarm,
+                size: 30,
+                color: Colors.black,
+              ),
+            ],
+          ),
         ),
       ),
     );

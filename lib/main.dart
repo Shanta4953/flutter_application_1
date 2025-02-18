f71ba3c9a3ad04d1985bc8e64b75ca28d45bc5ca
Shanta4953 <shanta15-4953@diu.edu.bd>
2025-02-18 09:41:20 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LabClass05());
+}
+
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home:Scaffold(
+        appBar:AppBar(
+          title: Text("Basic")
+        )
+        )
+      

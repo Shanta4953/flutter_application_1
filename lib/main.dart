434447c30c9f10d36209e56fb6f8786b0cdf8fc8
Shanta4953 <shanta15-4953@diu.edu.bd>
2025-02-18 10:40:33 +0600

    start

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,41 @@ void main() {
 class LabClass05 extends StatelessWidget {
   const LabClass05({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home:Scaffold(
-        appBar:AppBar(
-          title: Text("Basic")
-        )
-        )
-      
+      title: "Basic Flutter UI-02",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic Flutter UI-02"),
+          backgroundColor: Colors.blue[400],
+          centerTitle: true,
+        ),
+        body: Row(
+          children: [
+            Icon(
+             Icons.star,
+             size: 50, 
+            ),
+            Icon(
+             Icons.star,
+             size: 50, 
+            ),
+            Icon(
+             Icons.star,
+             size: 50, 
+            ),
+            Icon(
+             Icons.star,
+             size: 50, 
+            ),
+            Icon(
+             Icons.star_outline,
+             size: 50, 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
